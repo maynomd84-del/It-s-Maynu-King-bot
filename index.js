7d384fd910a39fa3ed2fcd0d84472b85996fa9e3
maynomd84-del <maynomd84@gmail.com>
2026-08-29 15:36:23 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const { default: makeWASocket, useMultiFileAuthState, DisconnectReason } = require('@whiskeysockets/baileys');
+const pino = require('pino');
+
+async function startBot() {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info');
+    
+    const sock = makeWASocket({
+        logger: pino({ level: 'silent' }),
+        printQRInTerminal: false,
+        auth: state
+    });
+
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update;
+        if (connection === 'close') {
+            const shouldReconnect = lastDisconnect?.error?.output?.statusCode !== DisconnectReason.loggedOut;
+            console.log('Connection closed. Reconnecting...', shouldReconnect);
+            if (shouldReconnect) {
+                startBot();
+            }
+        } else if (connection === 'open') {
+            console.log('Bot Connected Successfully as King Mainu!');
+        }
+    });
+
+    sock.ev.on('creds.update', saveCreds);
+}
+
+startBot();
